b754d2b091f42245605d3cff5bc09c5233a38302
github username <github email address>
2021-09-29 11:15:03 -0400

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,13 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+    fib_arr = [2]
+    first = 1
+    second = 2
+    while second + first < limit
+        temp = first + second
+        first = second
+        second = temp
+        fib_arr << second unless second % 2 != 0
+    end
+    fib_arr.inject(0, :+)
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,28 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+    attr_accessor :limit, :members
+
+    def initialize(limit)
+        @limit = limit
+        @members = gather_members
+    end
+
+    def gather_members
+        fib_arr = [2]
+        first = 1
+        second = 2
+        while second + first < limit
+            temp = first + second
+            first = second
+            second = temp
+            fib_arr << second unless second % 2 != 0
+        end
+
+        fib_arr
+    end
+
+    def sum
+        members.inject(0, :+)
+    end
+
+end
